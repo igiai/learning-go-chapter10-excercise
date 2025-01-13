b89636597ed12d822fb55abb5b576b579057244a
Michał Bernacki <michber575@gmail.com>
2025-01-13 14:37:04 +0100

    add docs

--- adder.go
+++ adder.go
@@ -1,5 +1,7 @@
+// Package adder is a simple package for adding numbers
 package adder
 
+// Add allows to add two integers and returns the result
 func Add(a, b int) int {
 	return a + b
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/igiai/learning-go-chapter10-excercise
+
+go 1.23.4
